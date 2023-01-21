872d43d5f876ee906b4d1b71d1fb56b0568d9e0c
Heitor Tanoue <tozaomello@gmail.com>
2023-01-21 19:57:31 -0300

    added PWA

--- components/gereral/metaTags.tsx
+++ components/gereral/metaTags.tsx
@@ -6,6 +6,17 @@ export default function MetaTags ({ children } : { children?: any }) {
             <title>Palola 🐾 - Não deixe seu cãozinho com fome</title>
             <link rel="icon" href="/icon.png" />
 
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+            />
+            <link rel="manifest" href="/manifest.json" /> 
+            <meta name="theme-color" content="lightcoral" />
+            <meta name="description" content="Palola é um dispositivo de alimentação para cães que permite programar horários para distribuir ração de forma automática, garantindo que o animal receba a quantidade certa de alimento em momentos predeterminados." />
+            <meta name="keywords" content="Palola, PET, Alimentação" />
+
             {/* <!--  Essential META Tags --> */}
             <meta property="og:title" content="Palola 🐾 - Não deixe seu cãozinho com fome"/>
             <meta property="og:image" content="https://palola.vercel.app/_next/image?url=palola-banner.png&w=256&q=100"/>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+    dest: 'public',
+})
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = module.exports = withPWA({
+    ...nextConfig
+})
