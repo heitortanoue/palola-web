3d87f348cf2a345693db23593980e46dcd1debc9
Heitor Tanoue <tozaomello@gmail.com>
2023-01-21 19:12:01 -0300

    fix lint redutant

--- components/gereral/toggle.tsx
+++ components/gereral/toggle.tsx
@@ -2,7 +2,7 @@ export default function Toggle ({ toggleState }: { toggleState: [boolean, (value
     return (
         <label htmlFor="toggle" className="flex items-center cursor-pointer relative">
             <input type="checkbox" id="toggle" className="sr-only" defaultChecked={!toggleState[0]}
-            onChange={(e) => toggleState[1](!Boolean(e.target.checked))}/>
+            onChange={(e) => toggleState[1](!e.target.checked)}/>
             <div className="toggleBg block bg-gray w-12 h-6 rounded-full"/>
             <div className="dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition"/>
         </label>
