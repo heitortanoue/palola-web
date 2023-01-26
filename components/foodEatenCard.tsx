a0248b05024cbe00a805c7d293f8d849ba992b62
Heitor Tanoue <heitortanoue@usp.br>
2023-01-26 16:35:53 -0300

    fixed padding on foodEatenCard

--- components/foodEatenCard.tsx
+++ components/foodEatenCard.tsx
@@ -19,8 +19,8 @@ export default function FoodEatenCard ({ meals, mealGroups } :
     : 0
 
     return (
-        <WhiteCard>
-            <div className="mb-2 font-medium text-sm flex justify-between">
+        <WhiteCard className="px-6">
+            <div className="mb-2.5 font-medium text-sm flex justify-between">
                 <div>
                     Xícaras de comida (hoje)
                 </div>
@@ -31,19 +31,19 @@ export default function FoodEatenCard ({ meals, mealGroups } :
             <div className="w-full bg-gray-lighter rounded-full h-2 mb-1.5">
                 <div className="bg-primary h-2 rounded-full relative font-medium text-xs"
                 style={{width: `${Math.min(percentage, 100)}%`}}>
-                    <div className="absolute inset-y-0 m-auto right-0 w-5 h-5
-                     rounded-full bg-white border-2 flex border-primary">
+                    <div className={`absolute inset-y-0 m-auto -right-2.5 
+                    w-5 h-5 rounded-full bg-white border-2 flex border-primary`}>
                         <i className="fa-solid fa-paw text-primary-dark m-auto"/>
                     </div>
 
-                    <div className="absolute inset-y-0 m-auto right-[0.15rem] text-gray mt-4">
+                    <div className="absolute inset-y-0 w-5 m-auto -right-2.5 text-center text-gray mt-4">
                         {todayMealsFoodEaten}
                     </div>
                 </div>
             </div>
             <div className="flex justify-between text-xs text-gray-light relative">
                 <div>
-                    0
+                    {percentage > 5 ? 0 : ""}
                 </div>
                 <div>
                     {percentage < 95 ? totalFoodToBeEatenToday : ""}
